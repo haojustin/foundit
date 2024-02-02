9d836776b7f99118a20c35689f4fe21df12b799e
eggzdee <victorli.freddy@gmail.com>
2024-02-01 23:33:56 -0800

    Added title TextInput and lost or found flair selector to post tab.

--- FoundIt/app/(tabs)/post.tsx
+++ FoundIt/app/(tabs)/post.tsx
@@ -1,11 +1,35 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Text, View } from '@/components/Themed';
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Post() {
+	const [title, setTitle] = React.useState('')
+	const [lostFound, setLostFound] = React.useState('lost')
+
 	return (
 		<View style={styles.container}>
-			
+			<View style={styles.inputElement}>
+				<TextInput
+					style={styles.textInput}
+					onChangeText={setTitle}
+					value={title}
+					placeholder="Title"
+					placeholderTextColor={colors.darkGray}
+					cursorColor={colors.lightGray}
+				/>	
+			</View>
+			<View style={styles.inputElement}>
+				<TouchableOpacity style={lostFound == 'lost' ? styles.lostFoundButtonSelected : styles.lostFoundButton} activeOpacity={0.5} onPress={() => setLostFound('lost')}>
+					<Text>
+						Lost
+					</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={lostFound == 'found' ? styles.lostFoundButtonSelected : styles.lostFoundButton} activeOpacity={0.5} onPress={() => setLostFound('found')}>
+					<Text>
+						Found
+					</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
@@ -24,5 +48,42 @@ const styles = StyleSheet.create({
 	text: {
 		fontSize: 15,
 		color: colors.lightGray,
-	}
+	},
+	inputElement: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		borderColor: 'red',
+		borderWidth: 0,
+	},
+	textInput: {
+		backgroundColor: 'black',
+		color: colors.lightGray,
+		fontSize: 20,
+		height: 50,
+		flex: 1,
+		fontWeight: 'bold',
+		marginLeft: 10,
+	},
+	lostFoundButton: {
+		margin: 10,
+		flex: 1,
+		height: 30,
+		backgroundColor: colors.darkGray,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderWidth: 2,
+		borderColor: 'transparent',
+		borderRadius: 5,
+	},
+	lostFoundButtonSelected: {
+		margin: 10,
+		flex: 1,
+		height: 30,
+		backgroundColor: colors.darkGray,
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderWidth: 2,
+		borderColor: colors.lightGray,
+		borderRadius: 5,
+	},
 });
