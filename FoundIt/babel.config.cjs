3eceeae15519e9f571182bf6a109b214689e346b
eggzdee <victorli.freddy@gmail.com>
2024-02-22 09:23:54 -0800

    Works fr this time.

--- FoundIt/babel.config.cjs
+++ FoundIt/babel.config.cjs
@@ -1,6 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}],'@babel/preset-typescript',"module:metro-react-native-babel-preset"]
+    presets: [['@babel/preset-env', {targets: {node: 'current'}}],["@babel/preset-react", {"runtime": "automatic"}],'@babel/preset-typescript',"module:metro-react-native-babel-preset"],
+	plugins: [["@babel/plugin-transform-react-jsx", {"runtime": "automatic"}]]
   };
 };
